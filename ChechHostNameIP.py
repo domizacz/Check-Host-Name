b0e0b7a179010f96303127d851d152316112ad40
Dom <Dom@ss.ss>
2020-05-19 15:45:47 -0500

    sec part

--- ChechHostNameIP.py
+++ ChechHostNameIP.py
@@ -5,13 +5,20 @@ import sys
 fileHandler = open("data.txt", "r")
 print("Program Start")
 line = fileHandler.readline()
-while line:
-# Get next line from file
-# If line is empty then end of file reached
-        ip2 =line.rstrip()
-        ip = socket.gethostbyname(ip2)
-        print("Host Name: {0} Ip: {1}".format(ip2, ip))
-        line = fileHandler.readline()
+if(line):
+        while line:
+        # Get next line from file
+        # If line is empty then end of file reached
+                ip2 =line.rstrip()
+                try:
+                    ip = socket.gethostbyname(ip2)
+                except OSError as error:
+                        print("Host not Find")
+                else:
+                        print("Host Name: {0} Ip: {1}".format(ip2, ip))
+                line = fileHandler.readline()
+else:
+        print ("File Empty")
 
 print ("Program end")
 # Close Close
